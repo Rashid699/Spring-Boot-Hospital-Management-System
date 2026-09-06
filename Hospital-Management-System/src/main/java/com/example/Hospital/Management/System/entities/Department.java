7e560350a9c23cbbc3524f44c913e756d2509629
Rashid <alshiblir650@gmail.com>
2026-09-06 11:10:54 +0400

    feat(Department): add no-args constructor

--- Hospital-Management-System/src/main/java/com/example/Hospital/Management/System/entities/Department.java
+++ Hospital-Management-System/src/main/java/com/example/Hospital/Management/System/entities/Department.java
@@ -3,10 +3,12 @@ package com.example.Hospital.Management.System.entities;
 
 import jakarta.persistence.Entity;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
 public class Department {
 }
