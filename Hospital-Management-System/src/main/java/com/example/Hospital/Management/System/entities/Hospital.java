bc20da6bfb89b94426e9f775b8da489303abdfbb
Rashid <alshiblir650@gmail.com>
2026-09-06 11:07:17 +0400

    feat(Department): add Entity annotation

--- Hospital-Management-System/src/main/java/com/example/Hospital/Management/System/entities/Hospital.java
+++ Hospital-Management-System/src/main/java/com/example/Hospital/Management/System/entities/Hospital.java
@@ -17,7 +17,7 @@ import java.util.List;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class Hospital {
+public class Hospital extends BaseClass {
 
     @Column(nullable = false, length = 100)
     private String name;
