38324a9bfdd6fb128bbd500e7910d1df57b37da0
Rashid <alshiblir650@gmail.com>
2026-09-06 09:53:44 +0400

    feat(BaseClass): setup BaseClass with getters and setters

---
+++ Hospital-Management-System/src/main/java/com/example/Hospital/Management/System/entities/BaseClass.java
@@ -0,0 +1,18 @@
+package com.example.Hospital.Management.System.entities;
+
+
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+
+@MappedSuperclass
+@Getter
+@Setter
+public class BaseClass {
+
+
+
+
+
+
+}
