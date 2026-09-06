ae2e6914bbe4d92a3ea803331d7693749e7a7873
Rashid <alshiblir650@gmail.com>
2026-09-06 14:57:29 +0400

    feat(Room): add no-args and all-args constructor

--- Hospital-Management-System/src/main/java/com/example/Hospital/Management/System/entities/Room.java
+++ Hospital-Management-System/src/main/java/com/example/Hospital/Management/System/entities/Room.java
@@ -2,11 +2,15 @@ package com.example.Hospital.Management.System.entities;
 
 
 import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
-public class Room {
+@NoArgsConstructor
+@AllArgsConstructor
+public class Room extends BaseClass{
 }
