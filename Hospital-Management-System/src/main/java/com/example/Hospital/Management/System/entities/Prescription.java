13329f761c90ff6c30fabac343b59daede8bb001
Rashid <alshiblir650@gmail.com>
2026-09-06 14:52:51 +0400

    feat(Prescription): add prescription duration days field

--- Hospital-Management-System/src/main/java/com/example/Hospital/Management/System/entities/Prescription.java
+++ Hospital-Management-System/src/main/java/com/example/Hospital/Management/System/entities/Prescription.java
@@ -21,4 +21,7 @@ public class Prescription extends BaseClass{
     @Column(nullable = false, length = 100)
     private String dosage;
 
+    @Column(nullable = false)
+    private Integer durationDays;
+
 }
