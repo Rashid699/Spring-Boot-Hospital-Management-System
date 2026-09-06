545e983d584e5b81937d23f5e21fad824c927ac6
Rashid <alshiblir650@gmail.com>
2026-09-06 12:17:44 +0400

    feat(Patient): add no-args and all-args constructor

--- Hospital-Management-System/src/main/java/com/example/Hospital/Management/System/entities/Appointment.java
+++ Hospital-Management-System/src/main/java/com/example/Hospital/Management/System/entities/Appointment.java
@@ -2,11 +2,15 @@ package com.example.Hospital.Management.System.entities;
 
 
 import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
-public class Appointment {
+@NoArgsConstructor
+@AllArgsConstructor
+public class Appointment extends BaseClass{
 }
