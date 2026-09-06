a4077fa893bb36dd219b968a7ebe669e9e1d269d
Rashid <alshiblir650@gmail.com>
2026-09-06 14:56:43 +0400

    feat(Room): add getter and setter annotation

--- Hospital-Management-System/src/main/java/com/example/Hospital/Management/System/entities/Room.java
+++ Hospital-Management-System/src/main/java/com/example/Hospital/Management/System/entities/Room.java
@@ -2,7 +2,11 @@ package com.example.Hospital.Management.System.entities;
 
 
 import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 public class Room {
 }
