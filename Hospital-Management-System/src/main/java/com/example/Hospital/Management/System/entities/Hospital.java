7642302a7b93581ee4ac1df69f1a3e8b6cc63281
Rashid <alshiblir650@gmail.com>
2026-09-06 10:59:41 +0400

    feat(Hospital): add hospital departments relationship

--- Hospital-Management-System/src/main/java/com/example/Hospital/Management/System/entities/Hospital.java
+++ Hospital-Management-System/src/main/java/com/example/Hospital/Management/System/entities/Hospital.java
@@ -3,10 +3,14 @@ package com.example.Hospital.Management.System.entities;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -20,5 +24,9 @@ public class Hospital {
 
     @Column(nullable = false, length = 150)
     private String location;
+
+    @OneToMany(mappedBy = "hospital")
+    private List<Department> departments = new ArrayList<>();
+
 
 }
