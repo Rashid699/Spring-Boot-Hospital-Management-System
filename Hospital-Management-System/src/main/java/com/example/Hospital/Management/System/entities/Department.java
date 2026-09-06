9e8e9bdf0940709852ba844ff17225b442cb2e99
Rashid <alshiblir650@gmail.com>
2026-09-06 11:27:21 +0400

    feat(Department): add department hospital relationship

--- Hospital-Management-System/src/main/java/com/example/Hospital/Management/System/entities/Department.java
+++ Hospital-Management-System/src/main/java/com/example/Hospital/Management/System/entities/Department.java
@@ -3,6 +3,8 @@ package com.example.Hospital.Management.System.entities;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,5 +22,9 @@ public class Department {
 
     @Column(length = 255)
     private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "hospital_id", nullable = false)
+    private Hospital hospital;
 
 }
