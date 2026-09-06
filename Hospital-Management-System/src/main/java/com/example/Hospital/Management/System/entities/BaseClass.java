1cd9b3c9404055f36b5ec3260d235e0cdaafeed1
Rashid <alshiblir650@gmail.com>
2026-09-06 10:26:46 +0400

    feat(BaseClass): add common fields to BaseClass

--- Hospital-Management-System/src/main/java/com/example/Hospital/Management/System/entities/BaseClass.java
+++ Hospital-Management-System/src/main/java/com/example/Hospital/Management/System/entities/BaseClass.java
@@ -1,14 +1,29 @@
 package com.example.Hospital.Management.System.entities;
 
 
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.time.LocalDateTime;
 
 @MappedSuperclass
 @Getter
 @Setter
 public class BaseClass {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Boolean isActive = true;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdDate;
+
+    private LocalDateTime updatedDate;
+
 
 
 
