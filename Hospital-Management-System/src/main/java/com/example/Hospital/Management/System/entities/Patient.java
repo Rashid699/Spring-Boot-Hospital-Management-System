66b92c5b2f27866aa6b7ab92c2c803e4db4b9b14
Rashid <alshiblir650@gmail.com>
2026-09-06 12:11:24 +0400

    feat(Patient): add patient medical records and patient guardians relationship

--- Hospital-Management-System/src/main/java/com/example/Hospital/Management/System/entities/Patient.java
+++ Hospital-Management-System/src/main/java/com/example/Hospital/Management/System/entities/Patient.java
@@ -35,4 +35,10 @@ public class Patient extends BaseClass{
 
     @OneToMany(mappedBy = "patient")
     private List<Appointment> appointments = new ArrayList<>();
+
+    @OneToMany(mappedBy = "patient")
+    private List<MedicalRecord> medicalRecords = new ArrayList<>();
+
+    @OneToMany(mappedBy = "patient")
+    private List<Guardian> guardians = new ArrayList<>();
 }
