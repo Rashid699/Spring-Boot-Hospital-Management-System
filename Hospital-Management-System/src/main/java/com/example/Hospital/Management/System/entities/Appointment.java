0bd0bc68e26a0795f6368a4d6b36f26736d2015c
Rashid <alshiblir650@gmail.com>
2026-09-06 12:16:27 +0400

    feat(Appointment): add setter and getter annotation

--- Hospital-Management-System/src/main/java/com/example/Hospital/Management/System/entities/Appointment.java
+++ Hospital-Management-System/src/main/java/com/example/Hospital/Management/System/entities/Appointment.java
@@ -3,8 +3,10 @@ package com.example.Hospital.Management.System.entities;
 
 import jakarta.persistence.Entity;
 import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Getter
+@Setter
 public class Appointment {
 }
