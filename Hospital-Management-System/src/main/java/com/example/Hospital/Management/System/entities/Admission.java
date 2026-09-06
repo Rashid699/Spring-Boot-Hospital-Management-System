3f05743a6e89bdb6da930f13478042f1d207794e
Rashid <alshiblir650@gmail.com>
2026-09-06 15:12:55 +0400

    feat(Admission): add no-args and all-args annotation

--- Hospital-Management-System/src/main/java/com/example/Hospital/Management/System/entities/Admission.java
+++ Hospital-Management-System/src/main/java/com/example/Hospital/Management/System/entities/Admission.java
@@ -2,11 +2,17 @@ package com.example.Hospital.Management.System.entities;
 
 
 import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
-public class Admission {
+@NoArgsConstructor
+@AllArgsConstructor
+public class Admission extends BaseClass{
+
+
 }
